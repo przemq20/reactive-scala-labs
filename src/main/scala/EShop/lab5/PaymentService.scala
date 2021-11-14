0e5cb7fa6ece0319833c864e33c67a443e3ad973
przemq20 <lechowiczprzemyslaw@gmail.com>
2021-11-14 16:32:35 +0100

    Added Payment and PaymentService

--- src/main/scala/EShop/lab5/PaymentService.scala
+++ src/main/scala/EShop/lab5/PaymentService.scala
@@ -1,10 +1,12 @@
 package EShop.lab5
 
-import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes}
+import akka.http.scaladsl.client.RequestBuilding.Get
+import akka.http.scaladsl.model.HttpResponse
 
+import scala.concurrent.ExecutionContextExecutor
 import scala.util.{Failure, Success}
 
 object PaymentService {
@@ -23,10 +25,28 @@ object PaymentService {
     payment: ActorRef[Response]
   ): Behavior[HttpResponse] =
     Behaviors.setup { context =>
-      ???
+      implicit val system: ActorSystem[Nothing]               = context.system
+      implicit val executionContext: ExecutionContextExecutor = system.executionContext
+
+      Http()
+        .singleRequest(Get(getURI(method)))
+        .onComplete {
+          case Success(response)  => context.self ! response
+          case Failure(exception) => throw exception
+        }
+
+      Behaviors.receiveMessage {
+        case HttpResponse(statusCode, _, _, _) if statusCode.intValue() == 200 =>
+          payment ! PaymentSucceeded
+          Behaviors.stopped
+        case HttpResponse(statusCode, _, _, _) if statusCode.intValue() >= 400 && statusCode.intValue() < 500 =>
+          throw PaymentClientError()
+        case HttpResponse(_, _, _, _) =>
+          throw PaymentServerError()
+      }
     }
 
-  // remember running PymentServiceServer() before trying payu based payments
+  // remember running PaymentServiceServer() before trying payu based payments
   private def getURI(method: String) =
     method match {
       case "payu"   => "http://127.0.0.1:8080"
